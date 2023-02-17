e9fa1e0d4357db49ba544ee737bdf1e71f08f0a2
student <you@example.com>
2023-02-17 16:21:32 +0300

    added comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,38 @@
 fun main() {
-        val school = "akirachix"
+    //Given a string “akirachix”. Write a function that prints out a string composed
+    //of the first, third and fourth characters of the string
+            val school = "akirachix"
+
    println(school[0] +""+school[2] +""+school[3] )
     println()
+
     info("Sharoni", 75)
     println()
+
     word("hotelTransilvania")
     word("GUI")
     println()
+
     Bool("Jenniffer", "Jenniffer")
     Bool("Washington", "Hudson")
 }
+//Write a function that takes in 2 parameters, name and age and returns a
+//String with this structure “Hi, my name is x and I am y years old.” Where x and
+//y are the provided name and age respectively.
 fun info(x: String, y: Int):String{
     var X = "Hi my name is " + x +" "+"and am "+y +"years old"
     println(X)
     return X
 }
+//Write a function that takes in a String and returns its length
 fun word(t: String){
     println(t.length)
 }
+//Write a function that takes in a name and prints out “That’s me!” when your
+//name is passed to it, otherwise it prints out “I don’t know who that is”
 fun Bool(name1: String, name2: String){
     if (name1==name2)
-        println("That's me")
+        println("That's me!")
     else
 
         println("I don't know who that is")
